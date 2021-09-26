201502a313ee4f9de8939f916221e77790c83ce0
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-09-26 15:08:51 +0200

    Add global css & SCSS compilation

--- rollup.config.js
+++ rollup.config.js
@@ -40,11 +40,13 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
+			preprocess: sveltePreprocess({
+				sourceMap: !production,
+			}),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
